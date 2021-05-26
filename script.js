50d4cfcaa9d794afb23c8ff368740316a0e62888
shiv42x <shivam2.patel@ryerson.ca>
2021-05-26 13:11:26 -0400

    finished game logic

--- script.js
+++ script.js
@@ -1,9 +1,45 @@
+let moves = ["rock", "paper", "scissors"]
+
 function computerPlay() {
-    let moves = ["Rock", "Paper", "Scissors"];
     let hand = Math.floor(Math.random() * 3);
-    console.log(moves[hand]);
+    return moves[hand];
 }
 
-for (var i = 0; i < 3; i++) { 
-    computerPlay();
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection == computerSelection) {
+        return "It's a draw!"
+    }
+    if (playerSelection == "rock") { 
+        if (computerSelection == "scissors") { 
+            return "You Win! Rock beats Scissors"
+        }
+        if (computerSelection == "paper") { 
+            return "You Lose! Paper beats Rock"
+        }
+    }
+    if (playerSelection == "paper") { 
+        if (computerSelection == "rock") { 
+            return "You Win! Paper beats Rock"
+        }
+        if (computerSelection == "scissors") { 
+            return "You Lose! Scissors beats Paper"
+        }
+    }
+    if (playerSelection == "scissors") { 
+        if (computerSelection == "paper") { 
+            return "You Win! Scissors beats Paper"
+        }
+        if (computerSelection == "rock") { 
+            return "You Lose! Rock beats Scissors"
+        }
+    }
+}
+
+
+for (let i = 0; i <= 5; i++) { 
+    playerSelection = window.prompt("Choose:", "");
+    computerSelection = computerPlay();
+    console.log("You chose " + playerSelection + ", Computer chose " + computerSelection);
+    console.log(playRound(playerSelection, computerSelection));
 }
